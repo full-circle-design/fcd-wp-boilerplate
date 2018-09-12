bde5053cab52363861acfc97d1e1b1018df3a868
John Stanhope <jstanhope@Johns-MacBook-Pro.local>
2018-09-12 12:17:26 -0400

    initial commit

---
+++ functions.php
@@ -0,0 +1,8 @@
+<?php
+
+// Enqueue css and js
+function fcd_boilerplate_scripts() {
+    wp_enqueue_style( 'style-name', get_stylesheet_uri() );
+    wp_enqueue_script( 'script-name', get_template_directory_uri() . '/js/example.js', array(), '1.0.0', true );
+}
+add_action( 'wp_enqueue_scripts', 'fcd_boilerplate_scripts' );
---
+++ gulpfile.js
@@ -0,0 +1,21 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var minify = require('gulp-minify');
+var concat = require('gulp-concat');
+var rename = require('gulp-rename');
+
+gulp.task('sass', function(){
+  return gulp.src('src/css/main.scss')
+    .pipe(sass())
+    .pipe(minify())
+    .pipe(rename('bundle.css'))
+    .pipe(gulp.dest('dist/css/')
+  )
+});
+
+gulp.task('scripts', function() {
+  return gulp.src('src/js/*.js')
+    .pipe(concat('bundle.js'))
+    .pipe(minify())
+    .pipe(gulp.dest('dist/js/'));
+});
